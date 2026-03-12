950f1cfc6ae63dead4f79fc932464e29e73fbeae
thaithuan123 <truonghoangthaithuan1102@gmail.com>
2026-03-12 19:18:08 +0700

    fix api users

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,45 @@
-const express = require('express');
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+
 const app = express();
-app.get('/', (req, res) => res.send('API Backend bán điẹn thoại đang chạy!'));
-app.listen(process.env.PORT || 3000, () => console.log('Server OK'));
+app.use(cors());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "tt2"
+});
+
+db.connect(err => {
+  if (err) {
+    console.log("Lỗi kết nối DB");
+  } else {
+    console.log("Kết nối DB thành công");
+  }
+});
+
+
+// lấy tất cả users
+app.get("/users", (req, res) => {
+  db.query("SELECT * FROM users", (err, result) => {
+    if (err) return res.send(err);
+    res.json(result);
+  });
+});
+
+
+// lấy user theo id
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+
+  db.query("SELECT * FROM users WHERE id=?", [id], (err, result) => {
+    if (err) return res.send(err);
+    res.json(result[0]);
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server chạy port 3000");
+});
